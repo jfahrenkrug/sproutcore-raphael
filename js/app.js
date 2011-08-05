cd744e043a49dd085ec92ffe4465d0fc3269764c
Johannes Fahrenkrug <jfahrenkrug@gmail.com>
2011-08-05 14:09:07 +0200

    some comments and cleanup

--- js/app.js
+++ js/app.js
@@ -1,8 +1,54 @@
-var App = SC.Application.create({
+var App = SC.Application.create({});
+
+// A class that represents an wraps an instance of a Raphael paper
+App.RaphaelPaper = SC.Object.extend({
+  width: 320,
+  height: 200,
+  path: null,
+  fill: "#000",
+  stroke: "black",
+  scale: 1,
+  elementId: null,
+  paperObject: null,
+  init: function() {
+    this._super();
+    // create and init the actual Raphael "paper" object
+    this.set('paperObject', Raphael(this.elementId, this.width, this.height));
+    // draw a path if one was given
+    if (!SC.empty(this.path)) {
+      this.pathDidChange();
+    }
+  },
+  pathDidChange: function() {
+    // clear the canvas
+    this.paperObject.clear();
+    // draw the path
+    var p = this.paperObject.path(this.path).attr({fill: this.fill, stroke: this.stroke});
+    // apply the scale
+    p.scale(this.scale, this.scale, 0, 0);
+  }.observes('path', 'fill', 'stroke', 'scale'),
+  sizeDidChange: function() {
+    // adjust the size of the Raphael canvas
+    this.paperObject.setSize(this.width, this.height);
+  }.observes('width', 'height'),
 });
 
+// our custom RaphaelView
+App.RaphaelView = SC.View.extend({
+  // This is called when the view element was inserted into the DOM
+  didInsertElement: function() {
+    // we'll bind the view's "paper" property to App.papers.<name-of-dom-element>
+    SC.bind(this, "paper", 'App.papers.' + this.get('elementId'));
+    // create an instance of App.RaphaelPaper, which in turn will create and init the Raphael object
+    App.papers.set(this.get('elementId'), App.RaphaelPaper.create({elementId: this.get('elementId'), path: this.get('path')}));
+  }
+});
+
+// an object that will hold all the App.RaphaelPaper objects
+// of our application, index by the DOM element name
 App.papers = SC.Object.create({});
 
+// A controller for "pasting" pre-fab SVG paths
 App.snippetsController = SC.Object.create({
   pasteOctocat: function() {
     SC.setPath("App.papers.main.path", Paths.Octocat); 
@@ -15,45 +61,5 @@ App.snippetsController = SC.Object.create({
   },
   pasteTwitter: function() {
     SC.setPath("App.papers.main.path", Paths.Twitter); 
-  }
-});
-
-App.RaphaelPaper = SC.Object.extend({
-  width: 320,
-  height: 200,
-  path: null,
-  fill: "#000",
-  stroke: "black",
-  scale: 1,
-  elementId: null,
-  paperObject: null,
-  init: function() {
-    this._super();
-    console.log('in init');
-    this.set('paperObject', Raphael(this.elementId, this.width, this.height));
-    if (!SC.empty(this.path)) {
-      this.pathDidChange();
-    }
-  },
-  pathDidChange: function() {
-    this.paperObject.clear();
-    var p = this.paperObject.path(this.path).attr({fill: this.fill, stroke: this.stroke});
-    p.scale(this.scale, this.scale, 0, 0);
-  }.observes('path', 'fill', 'stroke', 'scale'),
-  sizeDidChange: function() {
-    this.paperObject.setSize(this.width, this.height);
-  }.observes('width', 'height'),
-});
-
-
-App.RaphaelView = SC.View.extend({
-  mouseDown: function() {
-    console.log('paper', this.get('paper'));
-  },
-
-  didInsertElement: function() {
-    SC.bind(this, "paper", 'App.papers.' + this.get('elementId'));
-    App.papers.set(this.get('elementId'), App.RaphaelPaper.create({elementId: this.get('elementId'), path: this.get('path')}));
-    App.wurst = this;
   }
 });
